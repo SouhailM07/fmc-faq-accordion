a8521cf504c7d2363126f9798385222822edfac4
shadow <lightshadow416@gmail.com>
2023-12-25 22:14:10 +0100

    project core was build

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import(tailwindcss).Config} */ export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        lightPink: "#f1ebf1",
+        grayishPurple: "#7d6a80",
+        darkPurple: "#1a0d1e",
+      },
+    },
+  },
+  plugins: [],
+};
